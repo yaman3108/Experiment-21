1089d2e0d436b70e973cf3126d950edbb0aca954
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-10-22 13:23:29 +0530

    Create EXP21.2.cpp

---
+++ EXP21.2.cpp
@@ -0,0 +1,42 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int binary_search() {
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int mid;
+    int low;
+    int high;
+    int n;
+    cout<<"Enter the number to be searched"<<endl;
+    cin>>n;
+    low=0;
+    high=9;
+    while(low<=high){
+        mid=(low+high)/2;
+        if (arr[mid]<n){
+            low=mid+1;
+            
+        }
+        else if(arr[mid]>n){
+            high=mid-1;
+            
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int ans;
+    ans=binary_search();
+    if(ans!=-1){
+        cout<<"Number found at position "<<ans+1<<endl;
+    }
+    else{
+        cout<<"Number not found in the array"<<endl;
+    }
+    
+}
